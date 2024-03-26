0440346acfddb41b85de962960a6e1d0d60fee25
Nam Nguyen <nam.nguyen@shopify.com>
2024-03-26 17:33:13 -0300

    😎

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,10 @@ import { render } from "preact";
 import { App } from "./tooltip/App";
 import "./index.css";
 
+requestAnimationFrame(() => {
+  console.log("Hello from requestAnimationFrame!");
+});
+
+console.log("Hello from main.tsx!");
+
 render(<App />, document.getElementById("app")!);
