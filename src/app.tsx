974eb0256201dab8761409654111ec3d6573d391
Nam Nguyen <nam.nguyen@shopify.com>
2024-03-26 17:31:50 -0300

    Okela 🚀

--- src/app.tsx
+++ src/app.tsx
@@ -4,47 +4,19 @@ import viteLogo from "/vite.svg";
 import "./app.css";
 
 export function App() {
-  const [count, setCount] = useState(0);
+  const [width, setWidth] = useState(500);
 
   useEffect(() => {
-    console.log("Parent inside effect");
-  }, [count]);
+    // setWidth(1000);
+  }, []);
 
-  console.log("Parent outside effect");
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-
-      <Child count={count} />
-    </>
+    <div
+      style={{
+        width: width,
+        height: width,
+        backgroundColor: "red",
+      }}
+    ></div>
   );
-}
-
-function Child({ count }: { count: number }) {
-  useEffect(() => {
-    console.log("Child inside effect");
-  }, [count]);
-
-  console.log("Child outside effect");
-
-  return <h1>Child</h1>;
 }
