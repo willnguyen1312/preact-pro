75ab2da8bbb2000531d4de082ac5bb2881bdd0d8
Nam Nguyen <nam.nguyen@shopify.com>
2024-08-20 12:19:18 -0600

    Oke 🤘

---
+++ src/Signals.tsx
@@ -0,0 +1,16 @@
+import { signal, computed } from "@preact/signals";
+
+const count = signal(0);
+const double = computed(() => count.value * 2);
+
+export default function Counter() {
+  return (
+    <button
+      onClick={() => {
+        count.value++;
+      }}
+    >
+      {count} x 2 = {double}
+    </button>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { render } from "preact";
-import { App } from "./tooltip/App";
+import App from "./Signals";
 import "./index.css";
 
-requestAnimationFrame(() => {
-  console.log("Hello from requestAnimationFrame!");
-});
+// requestAnimationFrame(() => {
+//   console.log("Hello from requestAnimationFrame!");
+// });
 
-console.log("Hello from main.tsx!");
+// console.log("Hello from main.tsx!");
 
 render(<App />, document.getElementById("app")!);
